8b1b79ccb9312b789935a35f4edce472b2af71ad
Estudante <estudante@ifpr.edu.br>
2023-10-10 11:23:21 -0300

    Fiz algumas alterações

--- src/main/java/br/edu/ifpr/diagrama/CadastroMago.java
+++ src/main/java/br/edu/ifpr/diagrama/CadastroMago.java
@@ -9,12 +9,12 @@ package br.edu.ifpr.diagrama;
  *
  * @author Aluno
  */
-public class CadastroPersonagens extends javax.swing.JFrame {
+public class CadastroMago extends javax.swing.JFrame {
 
     /**
      * Creates new form CadastroPersonagem
      */
-    public CadastroPersonagens() {
+    public CadastroMago() {
         initComponents();
     }
 
@@ -82,14 +82,22 @@ public class CadastroPersonagens extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(CadastroPersonagens.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CadastroMago.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(CadastroPersonagens.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CadastroMago.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(CadastroPersonagens.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CadastroMago.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(CadastroPersonagens.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CadastroMago.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
         //</editor-fold>
@@ -102,7 +110,7 @@ public class CadastroPersonagens extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new CadastroPersonagens().setVisible(true);
+                new CadastroMago().setVisible(true);
             }
         });
     }
